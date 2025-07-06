2e108a2ad52f060fc79e051bb03f3b425c79c6f9
Utkarsh <the.utkarshofficial@gmail.com>
2025-07-07 01:05:08 +0530

    Create Day1_reverse_number.py
    Wrote a simple Python program to reverse a given number using a while loop. Practicing basic problem-solving and logic building.

---
+++ Day1_reverse_number.py
@@ -0,0 +1,9 @@
+num=12345
+rev=0
+#now looping the code to extract digits and storing them in a different variable (rev). So, that i could print it after the loop
+while num>0:
+  digit=num%10
+  rev=rev*10+digit
+  num=num//10
+print(rev)  
+  
