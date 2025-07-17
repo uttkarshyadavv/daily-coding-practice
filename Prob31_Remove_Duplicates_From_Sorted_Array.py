ab0ee6b95275717c6eab2f95ace60e8809c5576d
Utkarsh <the.utkarshofficial@gmail.com>
2025-07-17 21:48:38 +0530

    Prob31_Remove_Duplicates_From_Sorted_Array.py

--- Prob31_Remove_Duplicates_From_Sorted_Array.py
+++ Prob31_Remove_Duplicates_From_Sorted_Array.py
@@ -1,12 +1,9 @@
-#do not use for loop as the number of element varies after removing duplicate
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-n = len(nums)
-i = 0
-while i < len(nums) - 1:
+nums = [0, 1, 0, 1, 2, 2, 4, 3, 4, 4]
+for i in range(len(nums)):
     j = i + 1
-    if nums[i] == nums[j]:
-        nums.pop(j)  
-        continue 
-    i += 1
+    while j < len(nums):
+        if nums[i] == nums[j]:
+            nums.pop(j)
+        else:
+            j += 1 
 print(nums)
-print(len(nums))
