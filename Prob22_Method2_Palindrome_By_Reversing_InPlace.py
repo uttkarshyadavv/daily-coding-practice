572ebc58a57f24957284a0af35740569b2acad9b
Utkarsh <the.utkarshofficial@gmail.com>
2025-07-17 06:02:00 +0530

    Prob22_Method2_Palindrome_By_Reversing_InPlace.py

---
+++ Prob22_Method2_Palindrome_By_Reversing_InPlace.py
@@ -0,0 +1,15 @@
+original = "nitin"
+s = list(original)
+
+def rev(s, left, right):
+    if left >= right:
+        return
+    s[left], s[right] = s[right], s[left]
+    rev(s, left + 1, right - 1)
+
+rev(s, 0, len(s) - 1)
+
+if ''.join(s) == original:
+    print("Palindrome ✅")
+else:
+    print("Not a palindrome ❌")
