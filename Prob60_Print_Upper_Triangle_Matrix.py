23c117b7108d01f4efded4c4102ec4bae32ff508
Utkarsh <the.utkarshofficial@gmail.com>
2025-07-26 12:06:23 +0530

    Prob60_Print_Upper_Triangle_Matrix.py

---
+++ Prob60_Print_Upper_Triangle_Matrix.py
@@ -0,0 +1,11 @@
+nums = [
+    [5, 10, 8],
+    [7, 6, 3],
+    [2, 1, 9]
+]
+row = len(nums)
+column = len(nums[0])
+for i in range(row):
+    for j in range(column):
+        if i <= j:
+            print(nums[i][j])
