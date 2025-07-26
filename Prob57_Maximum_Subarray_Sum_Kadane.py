b02b7c02fd2d60f8b4388bb10d6c32dfb46ef117
Utkarsh <the.utkarshofficial@gmail.com>
2025-07-26 11:48:44 +0530

    Prob57_Maximum_Subarray_Sum_Kadane.py

---
+++ Prob57_Maximum_Subarray_Sum_Kadane.py
@@ -0,0 +1,14 @@
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+n = len(nums)
+total = 0
+maxi = float('-inf')
+
+for i in range(n):
+    total += nums[i]
+    if total < 0:
+        total = 0
+    elif total > maxi:
+        maxi = total
+
+print(maxi)  # Output: 6
