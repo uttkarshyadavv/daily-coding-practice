c288368f8667513ce9cb2b23872f6efb9de0c644
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-08-14 05:05:41 +0530

    Prob1_Reverse_Number

--- Prob1_Reverse_Number.py
+++ Prob1_Reverse_Number.py
@@ -1,4 +1,4 @@
-num=123456
+num=12345
 rev=0
 #now looping the code to extract digits and storing them in a different variable (rev). So, that i could print it after the loop
 while num>0:
@@ -6,4 +6,5 @@ while num>0:
   rev=rev*10+digit
   num=num//10
 print(rev)  
+
   
