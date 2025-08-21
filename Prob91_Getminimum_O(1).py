f0694b5d5d2a801f2fe2f407334aa931317cf37e
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-08-21 10:54:04 +0530

    Prob91_Getminimum_O(1)

---
+++ Prob91_Getminimum_O(1).py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self):
+        self.items = []
+        self.mini = None
+
+    def push(self, value):
+        if len(self.items) == 0:
+            self.items.append(value)
+            self.mini = value
+        else:
+            self.items.append(value)
+            if value < self.mini:
+                self.mini = value
+        return self.mini
