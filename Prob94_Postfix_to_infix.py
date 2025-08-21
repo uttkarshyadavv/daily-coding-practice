1786faec45e249946cd9ecf7071c2af6da09b565
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-08-22 00:59:17 +0530

    Prob94_Postfix_to_infix

---
+++ Prob94_Postfix_to_infix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def postToInfix(self,s):
+        stack=[]
+        for char in s:
+            # if character is an operand, push it to the stack
+            if char.isalnum():
+                stack.append(char)
+            else:
+                #Pop two operands
+                operand1= stack.pop()
+                operand2= stack.pop()
+                #Combine operands with a operator
+                new_exp= f"({operand1}{char}{operand2})"
+                #Push the result back onto the stack
+                stack.append(new_exp)
+        #The final element in the stack is the infix expression
+        return stack[-1]
