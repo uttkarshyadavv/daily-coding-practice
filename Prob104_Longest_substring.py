817aef80fe79f10343ec89e6997429b012a8fc1d
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-08-23 21:17:48 +0530

    Prob104_Longest_substring

--- Prob104_Longest_substring.py
+++ Prob104_Longest_substring.py
@@ -1,18 +1,30 @@
-s="CADBZABCD"
-n=len(s)
-freq={}
-stack=[]
-max=0
-for i in range(0,n):
+s = "CADBZABCD"
+n = len(s)
+freq = {}
+stack = []
+maxlen = 0
+
+for i in range(n):
     if s[i] not in freq:
-        freq[s[i]]=1
+        freq[s[i]] = 1
         stack.append(s[i])
+    elif len(s) == len(set(s)):
+        maxlen = len(s)
+        break
     else:
-        if max<len(stack):
-            max=len(stack)
-        freq={}
-        stack=[]
-print(max)
+        if maxlen < len(stack):
+            maxlen = len(stack)
+        freq = {}
+        stack = []
+        freq[s[i]] = 1
+        stack.append(s[i])
+
+# final check
+if maxlen < len(stack):
+    maxlen = len(stack)
+
+print(maxlen)
+
         
 
     
