fa6c0d44065c6bdceffab3e6ed0cf00bf2cbc586
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-08-29 22:23:15 +0530

    Prob119_Minimum_number_of_coin

--- Prob119_Minimum_number_of_coin.py
+++ Prob119_Minimum_number_of_coin.py
@@ -1,5 +1,33 @@
 coins=[1,2,5,10,20,50,100,200,500,2000]
 target=43
+i=0
+while i < len(coins):
+    if coins[i] > target:
+        coins.pop(i)
+    else:
+        i += 1
+print(coins)
+freq={}
+for i  in range(0,len(coins)):
+    if coins[i] not in freq:
+        freq[coins[i]]=1
+    else:
+        freq[coins[i]]+=1
+print(freq)
 currv=0
-for i in range(0,len(coins)):
+remaining=target
+used=[]
+while remaining>0 and len(coins)>0:
+    if coins[-1]<=remaining:
+        currv=currv+coins[-1]
+        used.append(coins[-1])
+        remaining=target-currv
+    else:
+        while remaining<coins[-1] and len(coins)>0:
+            del freq[coins[-1]]
+            coins.pop()
+print(used)
+
+
     
+
