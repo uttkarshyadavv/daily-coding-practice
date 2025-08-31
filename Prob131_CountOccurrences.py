5708eabda9a8686555ffb029e4347a48c136907d
Utkarsh <the.utkarshofficial@gmail.com>
2025-08-31 19:20:29 +0530

    Create Prob131_CountOccurrences.py

---
+++ Prob131_CountOccurrences.py
@@ -0,0 +1,31 @@
+#find the first and last occurance
+#nums[i]>=target (lb) and nums[i]>target (ub)
+def lb(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    lb=-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]<target:
+            low=mid+1
+        elif nums[mid]>=target:
+            lb=mid
+            high=mid-1       
+    return lb
+def ub(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    ub=-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]<=target:
+            low=mid+1
+        elif nums[mid]>target:
+            ub=mid
+            high=mid-1        
+    return ub
+lower=lb([5,7,7,8,8,8,8,8,10],7)
+upper=ub([5,7,7,8,8,8,8,8,10],7)
+print(upper-lower)
