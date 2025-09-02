15d9e7f1d235ffb1751cff612d435980e04c9615
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-03 04:06:48 +0530

    Prob203_Max_path_sum

--- Prob203_Max_path_sum.py
+++ Prob203_Max_path_sum.py
@@ -1,6 +1,8 @@
 maxi=float("-inf")
 def solution(node):
     global maxi
+    if node is None:
+        return 0
     ls=max(0,solution(node.left))
     rs=max(0,solution(node.right))
     maxi=max(maxi,ls+rs+node.val)
