e557e12b7fad4489ad15ae24d274cf2647975051
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-03 22:52:05 +0530

    Prob204_Top_view_BT

---
+++ Prob204_Top_view_BT.py
@@ -0,0 +1,19 @@
+from collections import deque
+def topView(self,root):
+    if not root:
+        return None
+    ans=[]
+    queue=deque()
+    result={}
+    queue.append(root,0)
+    while queue:
+        e,line=queue.popleft()
+        if line is not result:
+            result[line]=e.data
+        if e.left:
+            queue.append((e.left,line-1))
+        if e.right:
+            queue.append((e.right,line+1))
+    for value in sorted(result.items()):
+        ans.append(value[1])
+    return ans
