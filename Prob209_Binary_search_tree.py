488866c480a4b1284719a649b69324a2c0ddfa49
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-04 14:21:41 +0530

    Prob209_Binary_search_tree

--- Prob209_Binary_search_tree.py
+++ Prob209_Binary_search_tree.py
@@ -1,6 +1,7 @@
 #Binary search Tree: Left<=Node<=Right
 #Time Complexity=O(logn)
 #Search
+#In This The height is normally O(logn) [Special Case Exist] 
 def solution(node,val):
     if node is None:
         return None
