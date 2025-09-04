cdda24492f1947a66bc8cdc5cad306884cda08c5
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-04 15:12:12 +0530

    Prob213_Floor_BST.py

--- Prob213_Floor_BST.py
+++ Prob213_Floor_BST.py
@@ -1,10 +1,9 @@
-def solution(node,val):
+def solution(node, val):
     if node is None:
         return None
     if val == node.val:
         return node.val
-    elif node.val>val:
-        return solution(node.right,val)
-    else:
-        ceil=solution(node.left,val)
-    return ceil
+    elif node.val < val:
+        return solution(node.right, val) or node.val
+    else:  # node.val > val
+        return solution(node.left, val)
