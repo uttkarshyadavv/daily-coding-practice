b58ad79bc2e4a429d178ff6ba28def429f268f80
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-28 17:54:42 +0530

    Prob234_min_to_max_heap

--- Prob234_min_to_max_heap.py
+++ Prob234_min_to_max_heap.py
@@ -1,10 +1,22 @@
-arr=[2,4,7,6,10,9,11,9]
-n=len(arr)
-def heapify_up(self,arr,ind):
-    parent_Ind=(ind-1)//2
-    #if current index holds larger value than the parent
-    if ind>0 and arr[ind]<arr[parent_Ind]:
-        arr[ind],arr[parent_Ind]= arr[parent_Ind],arr[ind]
-        self.heapify_up(arr,parent_Ind)
-for i in range(n):
-    heapify_up(arr,i)
+arr = [2, 4, 7, 6, 10, 9, 11, 9]  # min-heap
+n = len(arr)
+
+def heapify_down(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify_down(arr, n, largest)
+
+# Convert min-heap to max-heap
+for i in range(n//2 - 1, -1, -1):
+    heapify_down(arr, n, i)
+
+print(arr)  # now arr is a max-heap
