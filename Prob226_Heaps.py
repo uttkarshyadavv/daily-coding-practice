8ddf11abc9fff033a746938d05fd8da4476ed6d5
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-28 12:25:18 +0530

    Prob226_Heaps

---
+++ Prob226_Heaps.py
@@ -0,0 +1,13 @@
+#Priority Queue
+#Complete BT: All elements have 2 childrens except the last level
+#Almost Complete BT: All elements have 2 children except the last and second last level all leaf node should start from extreme left
+#A max heap is a specialized tree-based data structure that satisfies the "max heap property." It's a type of binary heap where the main rule is that for any given node, its value must be greater than or equal to the values of its children.
+#A min heap: The main rule here is that for any given node, its value must be less than or equal to the values of its children.
+#Left Index: 2*index+1 and Right Index: 2*index+2 (For converting it into array/list)
+#Parent= LIF(index/2)-1
+#No. of Internal nodes= Leaf Nodes-1 (For Complete BT)
+#Index of Leaf node in almost complete BT: n/2 to n-1
+#Index of Internal node in almost complete BT: 0 to n/2 -1
+
+
+
