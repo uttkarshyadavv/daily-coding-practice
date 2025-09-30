065cf8b5e0692828360a0f74fe99c713d177d4df
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-09-30 14:29:05 +0530

    Prob241_Graph

---
+++ Prob241_Graph.py
@@ -0,0 +1,10 @@
+#Undirected Graph and Directed Graph
+#Node/Vertex
+#Directed Edge
+#Cycles in Graph: Starting from a node and ending at same node
+#Directed Cyclic graph and Directed Acyclic Graph
+#Path: Contanins nodes where all the nodes are reachable
+#Degree: No. of edges connected to a particular node
+#Total Degree= 2*Edges
+#Indgree: Towards that node, Outdegree: Away from that node
+#weights in Graph
