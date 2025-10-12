53c5bf2fff1e9a1f7f523233a9df68796f59cd34
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-10-13 04:24:33 +0530

    Prob255_Surrounded_region

---
+++ Prob255_Surrounded_region.py
@@ -0,0 +1,26 @@
+def dfs(r,c,visited,rows,column,board):
+    if r<0 or r>=rows or c<0 or c>=column:
+        return
+    if board[r][c]=="X":
+        return
+    if visited[r][c]==1:
+        return
+    visited[r][c]=1
+    dfs(r-1,c,visited,rows,column,board)
+    dfs(r,c-1,visited,rows,column,board)
+    dfs(r,c+1,visited,rows,column,board)
+    dfs(r+1,c,visited,rows,column,board)
+def solve(board):
+    rows=len(board)
+    column=len(board[0])
+    visited=[[0]*column for _ in range(rows)]
+    for r in range(rows):
+        for c in range(column):
+            if r==0 or c==0 or r==rows-1 or c==column-1:
+                if board[r][c]=="O":
+                    if visited[r][c]==0:
+                        dfs(r,c,visited,rows,column,board)
+    for r in range(rows):
+        for c in range(column):
+            if board[r][c]=="O" and visited[r][c]==0:
+                board[r][c]="X"
