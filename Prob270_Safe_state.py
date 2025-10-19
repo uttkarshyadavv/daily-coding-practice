c544ea47ee80106c20aa24107f0ddec20fc7c9a0
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-10-19 12:55:16 +0530

    Prob271_Safe_state_DFS

--- Prob270_Safe_state.py
+++ Prob270_Safe_state.py
@@ -1,26 +1,38 @@
 from collections import deque
-def safestate(graph,V,edge):
-    adj_list=[[] for _ in range(V)]
-    indegree=[0*V]
-    for node in range(V):
-        for adjnode in graph[node]:
-            adj_list[adjnode].append(node)
-    for node in range(len(adj_list)):
-        for adjNode in adj_list[node]:
-            indegree[adjNode]+=1
-    queue=deque()
-    result=[]
-    for i in range(V):
-        if indegree[i] == 0:
-            queue.append(i)
-    #It clears the nodes which are not having circular dependency
-    while len(queue)!=0:
-        node=queue.popleft()
-        result.append(node)
-        for adjNode in adj_list[node]:
-            indegree-=1
-            if indegree[adjNode]==0:
-                queue.append(adjNode)                                        
-    return sorted(result)
+def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+        V = len(graph)
+        adj_list = [[] for _ in range(V)]
+        indegree = [0 for _ in range(V)]
 
-    
+        # Reverse the graph
+        for node in range(V):
+            for adjnode in graph[node]:
+                adj_list[adjnode].append(node)
+
+        # Compute indegrees for reversed graph
+        for node in range(V):
+            for adjNode in adj_list[node]:
+                indegree[adjNode] += 1
+
+        queue = deque()
+        result = []
+
+        # Nodes with indegree 0 are initially safe
+        for i in range(V):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        # Kahn’s BFS
+        while queue:
+            node = queue.popleft()
+            result.append(node)
+            for adjNode in adj_list[node]:
+                indegree[adjNode] -= 1
+                if indegree[adjNode] == 0:
+                    queue.append(adjNode)
+
+        return sorted(result)
---
+++ Prob271_Safe_state_DFS.py
@@ -0,0 +1,31 @@
+def dfs(current_node,adj_list,vis,path_vis,is_safe):
+    vis[current_node]=1
+    path_vis[current_node]=1
+    for adjNode in adj_list[current_node]:
+        if vis[adjNode]==0:
+            ans= dfs(adjNode,adj_list,vis,path_vis,is_safe)
+            if ans==False:
+                return False
+            elif path_vis[adjNode]==1:
+                return False
+    is_safe[current_node]=1
+    path_vis[current_node]=0
+    return True
+def eventalSafeNodes(graph):
+    V=len(graph)
+    vis=[0 for _ in range(V)]
+    path_vis= [0 for _ in range(V)]
+    is_safe= [0 for _ in range(V)]
+
+    for i in range(V):
+        if vis[i]==0:
+            dfs(i,graph,vis,path_vis,is_safe)
+    result=[]
+    for i in range(V):
+        if is_safe[i]==1:
+            result.append(i)
+    return result
+
+
+
+    
