d8d7a2b3505d8da59a608bcf35e8ceb5bd3a3c75
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-10-19 16:26:28 +0530

    Prob272_Alien_Dict

--- Prob272_Alien_Dict.py
+++ Prob272_Alien_Dict.py
@@ -3,16 +3,15 @@ i, j = 0, 0
 a = len(words)
 result = []
 
-b = len(words[0])
+b = max(len(word) for word in words)  # longest word
 
 while i < a and j < b:
-    if j < len(words[i]):
-        if words[i][j] not in result: 
+    if j < len(words[i]):                # only access if index exists
+        if words[i][j] not in result:   # maintain order and uniqueness
             result.append(words[i][j])
     i += 1
-    if i == a - 1: 
+    if i == a - 1:
         i = 0
         j += 1
 
 print(result)
-
