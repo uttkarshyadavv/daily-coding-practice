c699f68644b3adbe9deed23180e9b4d81404d777
uttkarshyadavv <the.utkarshofficial@gmail.com>
2025-10-26 03:48:41 +0530

    Prob276_Diskstra's_Algo_set

--- Prob276_Diskstra's_Algo_set.py
+++ Prob276_Diskstra's_Algo_set.py
@@ -1,3 +1,4 @@
+#It's Bad because of time complexity O(V^2) whereas for the other method it's O(V)
 import sys
 def diskstra(V,edges,src):
     adj_list=[[] for _ in range(V)]
@@ -7,8 +8,8 @@ def diskstra(V,edges,src):
     distance[src]=0
     my_set=set()
     my_set.add((0,src))
-    while len(my_set)!=0:
-        dist,node=min(my_set)
+    while len(my_set)!=0: #O(V)
+        dist,node=min(my_set) #O(V)
         my_set.discard((dist,node))
         for adjNode, weight in adj_list[node]:
             dist_trav= dist+weight
